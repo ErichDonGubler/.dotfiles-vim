f1248e08cd68a3842190b917fc2e9a10cf8b6e4d
Erich Gubler <erichdongubler@gmail.com>
2020-11-11 15:13:08 -0700

    refactor(viminit): simplify `VIMINIT` significantly

--- .config/erichdongubler/sh/login/.vim.xdg.sh
+++ .config/erichdongubler/sh/login/.vim.xdg.sh
@@ -1,2 +1,2 @@
-export VIMINIT='let $MYVIMRC = $XDG_CONFIG_HOME . '"'"'/vim/vimrc'"'"' | source $MYVIMRC'
+export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 export WAKATIME_HOME="$XDG_CONFIG_HOME/wakatime"
